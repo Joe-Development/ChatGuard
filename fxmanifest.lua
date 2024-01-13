29566923295592dd64a49b07e677cf3e0bc6b9d4
Joe Development <146123038+Joe-Development@users.noreply.github.com>
2024-01-13 11:45:34 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'JoeV2'
+description 'A FiveM Chat Language FIlter Script'
+version '1.0.0'
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
