64865ae5074f71e56780f782ec32ebe701299e99
Joe Development <146123038+Joe-Development@users.noreply.github.com>
2024-01-13 11:43:48 -0500

    Added the Code For Server.lua

---
+++ server.lua
@@ -0,0 +1,37 @@
+local badWords = Config.badwords
+local discordWebhook = Config.badwords.discord.discordWebhook
+
+AddEventHandler('chatMessage', function(source, name, message)
+    for _, word in ipairs(badWords) do
+        if string.find(message, word, 1, true) then
+            local player = GetPlayerIdentifiers(source)
+            local playerName = GetPlayerName(source)
+            local playerDiscordId = nil
+            local playerFiveMId = nil
+
+            for _, identifier in ipairs(player) do
+                if string.find(identifier, "discord:") then
+                    playerDiscordId = string.gsub(identifier, "discord:", "")
+                elseif string.find(identifier, "fivem:") then
+                    playerFiveMId = string.gsub(identifier, "fivem:", "")
+                end
+            end
+
+            local embed = {
+                title = "Language Filter Alert",
+                fields = {
+                    { name = "Player: ", value = playerName },
+                    { name = "Discord ID: ", value = playerDiscordId },
+                    { name = "FiveM ID: ", value = playerFiveMId },
+                    { name = "Word/Sentence: ", value = word }
+                },
+                color = 16711680 
+            }
+
+            PerformHttpRequest(discordWebhook, function(statusCode, response, headers) end, 'POST', json.encode({embeds = {embed}}), { ['Content-Type'] = 'application/json' })
+            DropPlayer(source, "[LanguageFilter] You have been kicked for using [ " .. word .. " ] Please don't use it again.")
+            CancelEvent()
+            return
+        end
+    end
+end)
