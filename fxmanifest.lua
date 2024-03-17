624743307925b11ecc15a2681e9633ae4d0333c1
JoeV2 <146123038+Joe-Development@users.noreply.github.com>
2024-03-17 06:59:59 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,10 @@
 fx_version 'cerulean'
 games { 'gta5' }
 author 'JoeV2'
-description 'A FiveM Chat Language FIlter Script'
+description 'A Chat Guard To Secure Your Chat from any unwanted messages and spam'
 version '1.0.0'
 
 server_scripts {
-    'config.lua',
+    'Config.lua',
     'server.lua'
 }
